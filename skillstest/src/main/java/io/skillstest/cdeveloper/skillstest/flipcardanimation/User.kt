a73a19b6ade73dcb1abc1e8c8f1a61a23c6aab91
Condor Developer <cdeveloper@Condors-iMac.local>
2018-09-14 16:44:31 -0500

    Animation completed

---
+++ skillstest/src/main/java/io/skillstest/cdeveloper/skillstest/flipcardanimation/User.kt
@@ -0,0 +1,3 @@
+package io.skillstest.cdeveloper.skillstest.flipcardanimation
+
+data class User(val name: String)
--- skillstest/src/main/java/io/skillstest/cdeveloper/skillstest/util/Extensions.kt
+++ skillstest/src/main/java/io/skillstest/cdeveloper/skillstest/util/Extensions.kt
@@ -1,8 +1,15 @@
 package io.skillstest.cdeveloper.skillstest.util
 
 import android.graphics.Color
+import android.os.Build
+import android.support.design.widget.Snackbar
 import android.support.v4.widget.SwipeRefreshLayout
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.bumptech.glide.Glide
 
 fun ImageView.loadImageFromUrl(url: String): ImageView {
